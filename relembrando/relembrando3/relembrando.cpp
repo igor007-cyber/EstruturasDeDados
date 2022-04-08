040ed8009a8610bb7c8864772493b9f93672e375
igor ramalho <igor.ramalho007@gmail.com>
2022-04-08 18:51:18 -0300

    relembrando

---
+++ relembrando/relembrando3/relembrando.cpp
@@ -0,0 +1,20 @@
+// 1. Dados três números, imprimi-los em ordem crescente.   
+// 2. Dado os número P e Q, verifique se P é divisível por Q.
+
+/*
+
+3. Escreva um programa que calcule o salário de um profissional
+liberal baseado no valor da hora de serviço e da quantidade de 
+horas trabalhadas, deduzidos os descontos com INSS. O programa 
+deve ler os dados exigidos, calcular o valor do salário bruto e 
+do salário líquido, descontados os impostos e exibir todos os 
+valores lidos e calculados no final.
+
+4. A sequência de Fibonacci é calculada pela soma dos dois 
+últimos valores, ex.: 1, 1, 2, 3, 5, 8, 13, etc. Crie uma função 
+que receba do usuário um número N e imprima os N primeiros números
+da sequência.
+
+*/
+
+// 5. Faça uma função que leia um inteiro e calcule o seu fatorial.
